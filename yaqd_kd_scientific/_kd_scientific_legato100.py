8d57b498adb01cdd18e7610440733dbf449d93bd
Blaise Thompson <blaise@untzag.com>
2023-12-19 11:02:06 -0600

    working implementation, using aserial (#9)
    * working implementation, using aserial
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- yaqd_kd_scientific/_kd_scientific_legato100.py
+++ yaqd_kd_scientific/_kd_scientific_legato100.py
@@ -4,7 +4,7 @@ import asyncio
 import serial  # type: ignore
 from typing import Dict, Any, List
 
-from yaqd_core import IsDaemon, HasPosition, UsesSerial, IsDiscrete, UsesUart
+from yaqd_core import IsDaemon, HasPosition, UsesSerial, IsDiscrete, UsesUart, aserial
 
 
 class KdScientificLegato100(IsDiscrete, HasPosition, UsesUart, UsesSerial, IsDaemon):
@@ -12,21 +12,40 @@ class KdScientificLegato100(IsDiscrete, HasPosition, UsesUart, UsesSerial, IsDae
 
     def __init__(self, name, config, config_filepath):
         super().__init__(name, config, config_filepath)
-        # Perform any unique initialization
-        self._ser = serial.Serial("/dev/cu.usbmodem11075471")
-        # self._ser.baudrate = self._config["baud_rate"]
+        self._ser = aserial.ASerial(self._config["serial_port"])
+        self._ser.baudrate = self._config["baud_rate"]
+        self._ser.write("echo off\r\n".encode())
+        self._loop.create_task(self.poll_status())
+
+    def direct_serial_write(self, bytes) -> None:
+        self._ser.write(bytes)
+
+    async def poll_status(self):
+        while True:
+            self._ser.write("status\r\n".encode())
+            await asyncio.sleep(1)
 
     def _set_position(self, position) -> None:
         if position >= 0.5:
             # send serial command to START
-            self._state["position"] = self._state["destination"]
-            self._state["position_identifier"] = "run"
+            self._state["position"] = 1.0
+            self._state["position_identifier"] = "infusing"
             self._ser.write("run\r\n".encode())
         else:
-            self._state["position"] = self._state["destination"]
-            self._state["position_identifier"] = "stop"
+            self._state["position"] = 0.0
+            self._state["position_identifier"] = "paused"
             self._ser.write("stop\r\n".encode())
             # send serial command to STOP
 
-    def direct_serial_write(self, bytes) -> None:
-        self._ser.write(bytes)
+    async def update_state(self):
+        while True:
+            line = (await self._ser.areadline()).decode().strip()
+            if len(line) == 35:
+                motor_direction = line[29]
+                if motor_direction == "I":
+                    self._state["position"] = 1.0
+                    self._state["position_identifier"] = "infusing"
+                else:
+                    self._state["position"] = 0.0
+                    self._state["position_identifier"] = "paused"
+            await asyncio.sleep(0.1)
